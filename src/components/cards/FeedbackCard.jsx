1177fc3262fab987691aaa45ff39cfe8dfe33815
jipordev <srengchipor99@gmail.com>
2024-03-06 16:26:07 +0700

    feedback card

--- src/components/cards/FeedbackCard.jsx
+++ src/components/cards/FeedbackCard.jsx
@@ -1,34 +1,18 @@
-
-'use client';
-
-import { Avatar, Blockquote } from 'flowbite-react';
+import React from 'react';
+import { Card } from 'flowbite-react'; // Assuming you have imported Card component from flowbite-react
 
 function FBCard() {
   return (
-    <figure className="mx-auto w-[250px] text-center">
-      <svg
-        className="mx-auto mb-3 h-10 w-10 text-gray-400 dark:text-gray-600"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 18 14"
-      >
-        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-      </svg>
-      <Blockquote>
-        <p className="text-2xl font-medium italic text-gray-900 dark:text-white">
-          "Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to
-          complex dashboard. Perfect choice for your next SaaS application."
-        </p>
-      </Blockquote>
-      <figcaption className="mt-6 flex items-center justify-center space-x-3">
-        <Avatar rounded size="xs" img="https://media.wired.com/photos/650399af65d83ff288720473/1:1/w_1285,h_1285,c_limit/If-Elon-Musk-Had-Been-a-Happy-Child,-Would-He-Still-Be-Launching-Rockets--Business-Redux-h_16082330.jpg" alt="profile picture" />
-        <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-          <cite className="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</cite>
-          <cite className="pl-3 text-sm text-gray-500 dark:text-gray-400">CEO at Google</cite>
-        </div>
-      </figcaption>
-    </figure>
+    <Card href="#" className="w-[250px]">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Feedback from John Doe
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et justo eu lorem sagittis
+        condimentum eget quis lacus. Morbi vitae justo vel enim viverra porta non vitae turpis."
+      </p>
+    </Card>
   );
 }
-export default FBCard
+
+export default FBCard;
